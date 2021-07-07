ebf02f8f47869f2d5338c3bcf781d8d18c9d3310
vladmakarrov <vladmakarov22@gmail.com>
2021-07-07 16:00:28 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      browsersync = require('browser-sync').create(),
+      prettify = require('gulp-prettify');
+      kit = require('gulp-kit');
+
+function gulpKit() {
+  return gulp
+    .src('./kit/*.kit')
+    .pipe(kit())
+    .pipe(gulp.dest('./'));
+};
+
+function gulpSass() {
+  return gulp
+    .src('./styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'));
+}
+
+function serve(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    },
+    notify: false
+  }, done);
+}
+
+function reload(done) {
+  browsersync.reload();
+  done();
+}
+
+function prettifyFunc() {
+  return gulp
+    .src('*.html')
+    .pipe(prettify({
+      indent_size: 2,
+    }))
+    .pipe(gulp.dest('./'));
+}
+
+const compile = gulp
+  .series(
+    gulp.parallel(
+      gulp.series(
+        prettifyFunc,
+      ),
+      gulpKit,
+      gulpSass,
+    ),
+    serve,
+  );
+
+function watchFiles() {
+  gulp.watch('./styles/**/*.scss', gulp.series(gulpSass, reload));
+  gulp.watch('./kit/**/*.kit', gulp.series(gulpKit, reload));
+  gulp.watch('./**/*.html', gulp.series(reload));
+  gulp.watch('*.js', gulp.series(reload));
+}
+
+exports.watch = gulp.series(compile, watchFiles);
