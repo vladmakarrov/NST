d43adeec86be927161be7719be63e8719bee16e6
vladmakarrov <vladmakarov22@gmail.com>
2021-08-09 01:48:47 +0300

    Add Footer and fixed Header

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,28 @@
 const mobileMenu = document.querySelector('.mobile-menu');
-const regionDropdown = document.querySelector('.region-dropdown');
 const mobileMenuBtn = document.querySelector('.mobile-menu-button');
-const mobileMenuBtnClose = document.querySelector('.btn-close');
-const regionDropdownBtn = document.querySelector('.region-dropdown-btn');
+const copyrightCurrentYear = document.querySelector('.footer-copyright');
+const footerLogo = document.querySelector('.footer-logo-img')
 
 mobileMenuBtn.addEventListener('click', function(){
-    mobileMenu.classList.toggle('is-open');
+    mobileMenu.classList.toggle('active');
+    mobileMenuBtn.classList.toggle('active');
+    document.body.classList.toggle('menu-open');
 })
 
-mobileMenuBtnClose.addEventListener('click', function(){
-    mobileMenu.classList.toggle('is-open');
-})
+const currentYear = document.createElement('span');
+currentYear.textContent = new Date().getFullYear();
+copyrightCurrentYear.appendChild(currentYear);
 
-regionDropdownBtn.addEventListener('click', function(){
-    regionDropdown.classList.toggle('is-open');
-    regionDropdownBtn.textContent = '11';
-})
+function setFooterLogo () {
+    if (document.documentElement.clientWidth < 768) {
+        footerLogo.setAttribute('src', 'assets/img/logo_mobile.svg');
+    } else {
+        footerLogo.setAttribute('src', 'assets/img/logo.svg');
+    }
+}
+
+footerLogo.addEventListener('load', setFooterLogo());
+window.addEventListener('resize', setFooterLogo);
+
+
+
